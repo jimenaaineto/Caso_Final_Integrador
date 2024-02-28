22c17a8f2cd9c573a0d5825f923c21333716b630
jimenaaineto <159237388+jimenaaineto@users.noreply.github.com>
2024-02-28 08:59:55 +0000

    clasescreadas

---
+++ Caso_Final/src/main/resources/archetype-resources/src/main/java/Caso/Animales.java
@@ -0,0 +1,22 @@
+package Caso;
+public class Animales{
+    
+        private int edad;
+        private int tamaño;
+        private String origen;
+        private int nombre;
+
+    }
+
+    public class Terrestres extends Animales{
+        
+
+    }
+
+    public class Acuaticos extends Animales{
+
+    }
+
+    public class Aviarios extends Animales{
+        
+    }
--- Caso_Final/src/main/resources/archetype-resources/src/main/java/Caso/Habitat.java
+++ Caso_Final/src/main/resources/archetype-resources/src/main/java/Caso/Habitat.java
@@ -2,5 +2,36 @@ package Caso;
 
 public class Habitat{
 
-   
+        private int temperatura;
+        private int humedad;
+        private String limpieza;
+
+    
+    } 
+
+    public class HTerrestre extends Habitat{
+        public HTerrestre(int Ttemperatura, int Thumedad, String Tlimpieza){
+            this.temperatura=Ttemperatura;
+            this.humedad=Thumedad;
+            this.limpieza=Tlimpieza;
+        }
+
+    }
+
+    public class HAcuatico extends Habitat{
+        public HAcuatico(int Atemperatura, int Ahumedad, String Alimpieza){
+            this.temperatura=Atemperatura;
+            this.humedad=Ahumedad;
+            this.limpieza=Alimpieza;
+
+    }
+}
+
+    public class Haviario extends Habitat{
+        public HAviario(int Atemperatura, int Ahumedad, String Alimpieza){
+            this.temperatura=Atemperatura;
+            this.humedad=Ahumedad;
+            this.limpieza=Alimpieza;
+
+    }
 }
