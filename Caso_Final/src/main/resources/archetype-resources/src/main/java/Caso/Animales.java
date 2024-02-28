fd60a760c3be59f7ee014858811a4c57e8ab2737
jimenaaineto <159237388+jimenaaineto@users.noreply.github.com>
2024-02-28 19:01:17 +0000

    getters y setters animales

--- Caso_Final/src/main/resources/archetype-resources/src/main/java/Caso/Animales.java
+++ Caso_Final/src/main/resources/archetype-resources/src/main/java/Caso/Animales.java
@@ -1,24 +1,49 @@
 package Caso;
-public class Animales{
+
+public class Animales {
+    private int edad;
+    private String tamaño;
+    private String origen;
+    private String nombre;
+
+    public Animales(int edad, String tamaño, String origen, String nombre) {
+        this.edad = edad;
+        this.tamaño = tamaño;
+        this.origen = origen;
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
+
     
-        private int edad;
-        private int tamaño;
-        private String origen;
-        private int nombre;
-
-        public Animales (int edad)
-
-    }
-
-    public class Terrestres extends Animales{
-        
-
-    }
-
-    public class Acuaticos extends Animales{
-
-    }
-
-    public class Aviarios extends Animales{
-        
-    }
